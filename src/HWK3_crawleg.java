c6e264a2e0e06930856b5904ff4452dfab7f4df6
graemecrawley <gacrawley@gmail.com>
2015-11-20 15:55:29 -0500

    Added HWK3_crawleg and removed main

--- src/HWK3_crawleg.java
+++ src/HWK3_crawleg.java
@@ -1,12 +1,6 @@
-
 /*
  * Name: Gregory Smilski, Graeme Crawley, Alexandria Crump
  * MacID: smilsksi, crawleg, crumpal
  * Student Number: 1404091,  1417993, 1310858
  * Description:
  */
-
-
-public class main {
-
-}
