0d3e6a5e1db3db231016f4623e4d28c29bfd04b4
graemecrawley <gacrawley@gmail.com>
2015-11-21 15:39:46 -0500

    Made the scanner modular

--- src/HWK4_crawleg.java
+++ src/HWK4_crawleg.java
@@ -9,31 +9,28 @@ import java.util.Scanner;
 
 
 public class HWK4_crawleg{
-	public static void main(String[] args){
+	public static void main(String[] args){	
 		int n = 0;
-		
 		while(n != 3417){
 			try{
+			UserInterface s = new UserInterface();
+			s.changeCurrentPage(1);
 			//Login Screen
 			System.out.println("1. Sign in");
 			System.out.println("2. Sign up");
-			Scanner reader = new Scanner(System.in);  // Reading from System.in
 			System.out.print("Choose your option: ");
-			n = reader.nextInt(); // Scans the next token of the input as an int.
-			
+			n = Integer.parseInt(scanner());
 			//If Sign In
 			if (n==1){
 				System.out.print("Enter your username: ");
-				Scanner input2 = new Scanner(System.in);
-				String data2 = input2.nextLine();
-				System.out.println("");
+				String data = scanner();
 				//Scanning the Users.txt file...
-				if (search(data2, "Users.txt") == false){
+				if (search(data, "Users.txt") == false){
 					System.out.println("No Access"+"\n");
 					continue;
 				}
 				else{
-					System.out.println("Welcome "+data2+"\n");
+					System.out.println("Welcome "+data+"\n");
 					
 				}
 				
@@ -41,20 +38,18 @@ public class HWK4_crawleg{
 			//If Sign Up
 			if (n==2){
 				System.out.println("Choose your username:");
-				Scanner input3 = new Scanner(System.in);
-				String data3 = input3.nextLine();
-				System.out.println("");
+				String data = scanner();
 				//If exists
-				if(search(data3, "Users.txt")==true){
+				if(search(data, "Users.txt")==true){
 					System.out.println("This name already exists, please choose another"+ "\n");
 					continue; //TELL THEM IT ALREADY EXISTS AND HAVE THEM REENTER TO RETRY
 				}
-				//Uname doesn't exist
+				//User name doesn't exist
 				else{
 					try{
 						//Write to file
 						BufferedWriter bw = new BufferedWriter(new FileWriter("Users.txt", true));
-						bw.write(data3);
+						bw.write(data);
 						bw.newLine();
 					    bw.flush();
 						System.out.println("Username successfully added"+"\n");
@@ -90,17 +85,29 @@ public class HWK4_crawleg{
 				   final String lineFromFile = scanner.nextLine();
 				   //Check for blank 
 				   if(s=="" || s=="\n"){
+					   scanner.close();
 					   return false;
 				   }
 				   if(lineFromFile.contains(s)) {
+					   scanner.close();
 				       return true;
 				   }
+				   scanner.close();
 				}
 		}
+		
 		catch (FileNotFoundException e){
 			
 		}
+		
 		return false;
+	}
+	public static String scanner(){
+		Scanner scanner = new Scanner(System.in);
+		String scannerResult = scanner.nextLine();
+		System.out.println("");
+		scanner.close();
+		return scannerResult;
 	}
 }
 
