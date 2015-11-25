42bf7042b7ada5318699c0fc5c2523466eaeedd9
Alie Crump <alie.crump@gmail.com>
2015-11-24 22:41:49 -0500

    hey

--- src/HWK4_crawleg.java
+++ src/HWK4_crawleg.java
@@ -1,3 +1,4 @@
+
 /*
  * Name: Gregory Smilski, Graeme Crawley, Alexandria Crump
  * MacID: smilsksi, crawleg, crumpal
@@ -8,20 +9,17 @@ import java.io.*;
 import java.util.Scanner;
 
 public class HWK4_crawleg {
-	
-	public static ShoppingCart[] Contents;
-	
 	public static void main(String[] args) throws IOException {
 		int n = 0;
 		Scanner inputScanner = new Scanner(System.in);
 		String scannerResult = "";
-		UserInterface r = new UserInterface();
+		UserInterface s = new UserInterface();
 		User u = new User();
-		r.changeCurrentPage(1);
+		s.changeCurrentPage(1);
 		while (n != 3417) {
 			try {
 				//Login Page
-				if (r.getCurrentPage() == 1) {
+				if (s.getCurrentPage() == 1) {
 					System.out.println("1. Sign in");
 					System.out.println("2. Sign up");
 					System.out.print("Choose your option: ");
@@ -30,24 +28,24 @@ public class HWK4_crawleg {
 					n = Integer.parseInt(scannerResult);
 					// If Sign In
 					if (n == 1) {
-						r.changeCurrentPage(3);
+						s.changeCurrentPage(3);
 						continue;
 					}
 					// If Sign Up
 					if (n == 2) {
-						r.changeCurrentPage(2);
+						s.changeCurrentPage(2);
 						continue;
 					}
 
 				}
 				//Choose Username
-				if(r.getCurrentPage()==2){
+				if(s.getCurrentPage()==2){
 					System.out.println("Choose your username:");
 					scannerResult = inputScanner.nextLine();
 					System.out.println("");
 					String data = scannerResult;
 					// If exists
-					if (r.search(data, "Users.txt") == true) {
+					if (search(data, "Users.txt") == true) {
 						System.out.println("This name already exists, please choose another" + "\n");
 						continue;
 					}
@@ -55,36 +53,30 @@ public class HWK4_crawleg {
 					else {
 						u.getUsername(data);
 						User.createUsername(data);
-						u.createCart(data, 0);
-						UserInterface.initCart(u.returnUsername());
-						ShoppingCart.setContents(Contents);
-						r.changeCurrentPage(1);
+						s.changeCurrentPage(1);
 					}
 				}
-				if(r.getCurrentPage()==3){
+				if(s.getCurrentPage()==3){
 					System.out.print("Enter your username: ");
 					scannerResult = inputScanner.nextLine();
 					System.out.println("");
 					String data = scannerResult;
-					// Scanning the Userr.txt file...
-					if (r.search(data, "Users.txt") == false) {
-						r.changeCurrentPage(4);
+					// Scanning the Users.txt file...
+					if (search(data, "Users.txt") == false) {
+						s.changeCurrentPage(4);
 						continue;
 					} else {
-						u.getUsername(data);
-						u.createCart(data, 1);
-						UserInterface.initCart(u.returnUsername());
-						ShoppingCart.setContents(Contents);
-						r.changeCurrentPage(5);
+						System.out.println("Welcome " + data + "\n");
+						s.changeCurrentPage(5);
 						continue;
 					}
 				}
-				if(r.getCurrentPage()==4){
+				if(s.getCurrentPage()==4){
 					System.out.println("No Access" + "\n");
-					r.changeCurrentPage(1);
+					s.changeCurrentPage(1);
 					continue;
 				}
-				if(r.getCurrentPage()==5){
+				if(s.getCurrentPage()==5){
 					System.out.println("1. View Items By Category");
 					System.out.println("2. View Shopping Cart");
 					System.out.println("3. Sign Out");
@@ -94,21 +86,21 @@ public class HWK4_crawleg {
 					n = Integer.parseInt(scannerResult);
 					// If Sign In
 					if (n == 1) {
-						r.changeCurrentPage(6);
+						s.changeCurrentPage(6);
 						continue;
 					}
 					// If Sign Up
 					if (n == 2) {
-						r.changeCurrentPage(7);
+						s.changeCurrentPage(7);
 						continue;
 					}
 					if (n == 3) {
-						r.changeCurrentPage(1);
+						s.changeCurrentPage(1);
 						continue;
 					}
 					
 				}
-				if(r.getCurrentPage()==6){
+				if(s.getCurrentPage()==6){
 					System.out.println("1. Readables");
 					System.out.println("2. Audio");
 					System.out.println("");
@@ -119,67 +111,33 @@ public class HWK4_crawleg {
 					scannerResult = inputScanner.nextLine();
 					System.out.println("");
 					n = Integer.parseInt(scannerResult);
-					// If Sign In
 					if (n == 1) {
-						r.changeCurrentPage(8);
+						s.changeCurrentPage(8);
 						continue;
 					}
-					// If Sign Up
 					if (n == 2) {
-						r.changeCurrentPage(9);
+						s.changeCurrentPage(9);
 						continue;
 					}
 					if (n == -1) {
-						r.changeCurrentPage(5);
+						s.changeCurrentPage(5);
 						continue;
 					}
 				}
-				if(r.getCurrentPage()==7){
-					System.out.println("Shopping Cart");
-					System.out.println("");
-					ShoppingCart.displayContents(u.returnUsername());
-					System.out.println("");
-					System.out.println("Press -1 to return to");
-					System.out.println("the previous menu");
-					scannerResult = inputScanner.nextLine();
-					System.out.println("");
-					n = Integer.parseInt(scannerResult);
-					if (n == -1) {
-						r.changeCurrentPage(5);
-						continue;
-					}
-				}
-
-				if(r.getCurrentPage()==8){
-					System.out.println("Readables");
-					System.out.println("");
-					System.out.println("");
-					System.out.println("Choose your option: ");
-					System.out.println("");
-					System.out.println("Press -1 to return to");
-					System.out.println("the previous menu");
-					scannerResult = inputScanner.nextLine();
-					System.out.println("");
-					n = Integer.parseInt(scannerResult);
-					// If Sign In
-					if (n == 1) {
-						r.changeCurrentPage(8);
-						continue;
-					}
-					//Assume they've chosen an item
-					else{
-						//check to make sure the number they've entered is valid for the selected type ie audio/readable
-						//
-					}
-					r.changeCurrentPage(1);
+				if(s.getCurrentPage()==7){
+					s.changeCurrentPage(1);
 					continue;
 				}
-				if(r.getCurrentPage()==9){
-					r.changeCurrentPage(1);
+				if(s.getCurrentPage()==8){
+					s.showReadables();
+					break;
+				}
+				if(s.getCurrentPage()==9){
+					s.changeCurrentPage(1);
 					continue;
 				}
-				if(r.getCurrentPage()==10){
-					r.changeCurrentPage(1);
+				if(s.getCurrentPage()==10){
+					s.changeCurrentPage(1);
 					continue;
 				}
 
@@ -193,5 +151,33 @@ public class HWK4_crawleg {
 		}
 		inputScanner.close();
 
+	}
+
+	// Function to search file for string
+	public static boolean search(String s, String f) {
+		File file = new File(f);
+		try {
+			final Scanner fileScanner = new Scanner(file);
+			while (fileScanner.hasNextLine()) {
+				final String lineFromFile = fileScanner.nextLine();
+				// Check for blank
+				if (s == "" || s == "\n") {
+					fileScanner.close();
+					return false;
+				}
+				if (lineFromFile.contains(s)) {
+					fileScanner.close();
+					return true;
+				} else {
+					continue;
+				}
+			}
+			fileScanner.close();
+		}
+
+		catch (FileNotFoundException e) {
+
+		}
+		return false;
 	}
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -11,11 +11,7 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 public class UserInterface {
@@ -23,39 +19,48 @@ public class UserInterface {
 	private Array readables;
 	@SuppressWarnings("unused")
 	private Array audioProducts;
-	private int currentPage; // the page number (P1..P10)
+	private int currentPage;// the page number (P1..P10)
 	public int getCurrentPage(){ //This method is for page navigation. Based on the values of the state variable, call different pages
 		return currentPage;
 	}
 	public void changeCurrentPage(int n){// This method is for page navigation. It should change to current page and show the content.
 		currentPage = n;
 	}
-	private String[] bookInfo;
-	private Book[] bookArray;
-	private String[] eBookInfo;
-	private eBook[] eBookArray;
-	public void getReadables() throws IOException {// Fetches all readable from the files and places them in the readable array
-		//get books from Books.txt		
-		bookInfo = readLines("Books.txt"); //seperates lines into strings in an array
-		bookArray = new Book[bookInfo.length];
-		for (int i = 0; i < bookInfo.length; i++){ 
-			String item = bookInfo[i]; 
-			String[] tempArray = item.split(",");
-			bookArray[i] = new Book(Integer.parseInt(tempArray[0]), tempArray[1], tempArray[2], Double.parseDouble(tempArray[3]), Integer.parseInt(tempArray[4]));
-		}
+	public Readable[] getReadables() throws IOException {// Fetches all readable from the files and places them in the readable array
+		
 		//get eBooks from eBook.txt
-		eBookInfo = readLines("Ebooks.txt");
-		eBookArray = new eBook[eBookInfo.length];
-		for (int j = 0; j< eBookInfo.length; j++){
+		String[] eBookInfo = readLines("Ebooks.txt");
+		eBook[] eBookArray = new eBook[eBookInfo.length];
+		for (int j = 0; j< 2/*eBookInfo.length*/; j++){
 			String item = eBookInfo[j];
 			String[] tempArray = item.split(",");
-			eBookArray[j] = new eBook(Integer.parseInt(tempArray[0]), tempArray[1], tempArray[2], Double.parseDouble(tempArray[3]), Integer.parseInt(tempArray[4]));
-		}		
+			System.out.println("j: "+j);
+			System.out.println("ebookinfo: "+ eBookInfo.length);
+			eBookArray[j] = new eBook(3, "name", "author", 100, 10); //Integer.parseInt(tempArray[0]), tempArray[1], tempArray[2], Double.parseDouble(tempArray[3]), Integer.parseInt(tempArray[4])
+		
+		}
+		//get books from Books.txt		
+		String[] bookInfo = readLines("Books.txt"); //separates lines into strings in an array
+		Book[] bookArray = new Book[bookInfo.length];
+			for (int i = 0; i < bookInfo.length; i++){ 
+				//String item = bookInfo[i]; 
+				//String[] tempArray = item.split(",");
+				//bookArray[i] = new Book(Integer.parseInt(tempArray[0]), tempArray[1], tempArray[2], Double.parseDouble(tempArray[3]), Integer.parseInt(tempArray[4]));
+			}
+		Readable[] readableArray = new Readable[bookArray.length + eBookArray.length];
+		int i;
+		int j = 0;
+		for (i=0;i<(bookArray.length+eBookArray.length);i++){
+			if (i<bookArray.length){
+				readableArray[i] = bookArray[i];
+			}else{
+				readableArray[i] = eBookArray[j];
+				j++;
+			}
+		}
+				
+		return null; //readableArray;		
 	}
-	private String[] CDInfo;
-	private CD[] CDArray;
-	private String[] MP3Info;
-	private MP3[] MP3Array;
 	public void getAudioProducts() throws IOException{ // Fetches all audio products from the files and places in them in the readables array
 		//get CDs from CDs.txt
 		String[] CDInfo = readLines("CD.txt"); //seperates lines into strings in an array
@@ -74,20 +79,17 @@ public class UserInterface {
 			MP3Array[j] = new MP3(Integer.parseInt(tempArray[0]), tempArray[1], tempArray[2], Double.parseDouble(tempArray[3]), Integer.parseInt(tempArray[4]));
 		}	
 	}
-	public void showReadables(){ //Displays all readable for browsing
+	public void showReadables() throws IOException{ //Displays all readable for browsing
 		System.out.println(String.format("%-4s%-16s%-16s%-5s%-8s%-6s","sNo","Name","Author", "Price", "Quantity", "Type"));
-		for (int i = 0; i<this.bookArray.length; i++){
-			Book book = bookArray[i];
-			String type = "book";
-			System.out.println(String.format("%4d%16s%16s%5d%4d%6s", book.sNo, book.name, book.authorName, book.price, type));
-		}
-		for (int i = 0; i<this.eBookArray.length; i++){
-			eBook book = eBookArray[i];
-			String type = "eBook";
-			System.out.println(String.format("%4d%16s%16s%5d%4d%6s", book.sNo, book.name, book.authorName, book.price, type));
-		}
+		Readable[] readableArray = getReadables();
+		int length = readableArray.length;
+		/*for (int i = 0; i<length; i++){
+			Readable item = readableArray[i];
+			
+			System.out.println(String.format("%4d%16s%16s%5d%4d%6s", item.sNo, item.name, item.authorName, item.price, item.type));
+		}*/
 	}
-	public void showAudioProducts(){ // Displays all audio products for browsing
+	/*public void showAudioProducts(){ // Displays all audio products for browsing
 		System.out.println(String.format("%4d%16s%16s%5d%4d%6s","sNo","Name","Artist", "Price", "Type"));
 		for (int i = 0; i<this.CDArray.length; i++){
 			CD cd = CDArray[i];
@@ -99,7 +101,7 @@ public class UserInterface {
 			String type = "eBook";
 			System.out.println(String.format("%4d%16s%16s%5d%4d%6s", book.sNo, book.name, book.authorName, book.price, type));
 		}
-	}
+	}*/
 	public boolean search(String s, String f) {
 		File file = new File(f);
 		try {
@@ -126,18 +128,34 @@ public class UserInterface {
 		}
 		return false;
 	}
-	public static String[] readLines(String filename) throws IOException{
-		FileReader fileReader = new FileReader(filename);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
-		List<String> lines = new ArrayList<String>();
-		String line = null;
-		while ((line = bufferedReader.readLine()) != null){
-			lines.add(line);
-			}
-		bufferedReader.close();
-		String[] linesArray = new String[lines.size()];
-		linesArray = lines.toArray(linesArray);
-		return linesArray;
+	public static String[] readLines(String filename) throws IOException, FileNotFoundException{
+		System.out.println("1");
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+		try{
+			System.out.println(filename);
+			System.out.println(new File(filename).getAbsolutePath());
+			fileReader = new FileReader(filename);
+			System.out.println(filename);
+			System.out.println("2");
+			bufferedReader = new BufferedReader(fileReader);
+			System.out.println("3");
+			List<String> lines = new ArrayList<String>();
+			System.out.println("4");
+			String line = null;
+			System.out.println("5");
+			while ((line = bufferedReader.readLine()) != null){
+				lines.add(line);
+				System.out.println("6");
+				}
+			bufferedReader.close();
+			String[] linesArray = new String[lines.size()];
+			linesArray = lines.toArray(linesArray);
+			return linesArray;
+		}catch(FileNotFoundException e){
+			System.out.println("FileNotFound");
+		}
+		return null;
 		}
 	
 	public static void initCart(String u) throws IOException{
